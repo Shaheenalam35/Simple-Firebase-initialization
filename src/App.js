d302c8e2ad45d25628bdfffc50e7665e926f0fe7
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-04-07 10:46:54 +0200

    Sign in and sign out with google implement

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
-    console.log("working on google");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
   };
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button onClick={handleSignOut}>Sign Out</button>
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your email Address: {user.email}</p>
+      <img src={user.photoURL} alt=" Photo nei" />
     </div>
   );
 }
